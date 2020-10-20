f24bfe18b1846f084b167a6ea47ee5b6841b4a26
Sulakshana Ranawake <sranawake@LKCOMSRANAWAK01.local>
2020-10-20 15:39:39 +0530

    user added to db at signup

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth} from '@angular/fire/auth';
+import { AngularFirestore} from '@angular/fire/firestore';
 // import { rejects } from 'assert';
 // import { resolve } from 'dns';
 
@@ -9,12 +10,18 @@ import { AngularFireAuth} from '@angular/fire/auth';
 export class AuthenticationService {
 
   constructor(
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private db: AngularFirestore
   ) { }
 
   signupUser(value){
     return new Promise<any>((resolve,reject) => {
-      this.afAuth.createUserWithEmailAndPassword(value.email, value.password).then(res => resolve(res), err=> reject(err))
+      this.afAuth.createUserWithEmailAndPassword(value.email, value.password).then(res => {
+        resolve(res);
+        this.db.collection("users").add({
+          userEmail : value.email
+        })
+      }, err=> reject(err))
     })
   }
 
@@ -45,6 +52,6 @@ export class AuthenticationService {
   }
 
   userList(){
-
+    return this.db.collection("users").get();
   }
 }
